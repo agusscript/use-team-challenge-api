4c5affb87fe51a9e106784bab58db8abdd51a69a
agusscript <agus.sanchez.240@gmail.com>
2025-05-08 10:15:22 -0300

    Add main app configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe, VersioningType } from "@nestjs/common";
+import { NestFactory, Reflector } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+
+  const globalPrefix = process.env.GLOBAL_PREFIX ?? "api";
+  app.setGlobalPrefix(globalPrefix);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: "1",
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector))
+  );
+ 
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
 }
 bootstrap();

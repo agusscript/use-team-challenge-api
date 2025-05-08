93d744ad6c6904699f8127a09c87a086960fff67
agusscript <agus.sanchez.240@gmail.com>
2025-05-08 18:10:42 -0300

    Add repository layer for column module

---
+++ src/module/column/repository/column.repository.ts
@@ -0,0 +1,52 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Column } from '../entity/column.entity';
+
+@Injectable()
+export class ColumnRepository {
+  constructor(
+    @InjectRepository(Column)
+    private readonly repository: Repository<Column>
+  ) { }
+
+  async findAll(): Promise<Column[]> {
+    return await this.repository.find({
+      relations: { cards: true }
+    });
+  }
+
+  async findOneById(id: string): Promise<Column> {
+    const column = await this.repository.findOne({
+      where: { id },
+      relations: { cards: true }
+    });
+
+    if (!column) {
+      throw new NotFoundException(`Column with ID ${id} not found`);
+    }
+
+    return column;
+  }
+
+  async create(column: Column): Promise<Column> {
+    return await this.repository.save(column);
+  }
+
+  async update(id: string, updates: Partial<Column>): Promise<Column> {
+    const columnToUpdate = await this.repository.preload({
+      ...updates,
+      id,
+    });
+
+    if (!columnToUpdate) {
+      throw new NotFoundException(`Column with ID ${id} not found`);
+    }
+
+    return await this.repository.save(columnToUpdate);
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.repository.delete(id);
+  }
+}

2621008dce87fd548176433495c9b3bf5faafa0e
agusscript <agus.sanchez.240@gmail.com>
2025-05-08 18:13:15 -0300

    Add repository layer for card module

---
+++ src/module/card/repository/card.repository.ts
@@ -0,0 +1,49 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Card } from '../entity/card.entity';
+
+@Injectable()
+export class CardRepository {
+  constructor(
+    @InjectRepository(Card)
+    private readonly repository: Repository<Card>
+  ) { }
+
+  async findAll(): Promise<Card[]> {
+    return await this.repository.find();
+  }
+
+  async findOneById(id: string): Promise<Card> {
+    const card = await this.repository.findOne({
+      where: { id },
+    });
+
+    if (!card) {
+      throw new NotFoundException(`Card with ID ${id} not found`);
+    }
+
+    return card;
+  }
+
+  async create(card: Card): Promise<Card> {
+    return await this.repository.save(card);
+  }
+
+  async update(id: string, updates: Partial<Card>): Promise<Card> {
+    const cardToUpdate = await this.repository.preload({
+      ...updates,
+      id,
+    });
+
+    if (!cardToUpdate) {
+      throw new NotFoundException(`Card with ID ${id} not found`);
+    }
+
+    return await this.repository.save(cardToUpdate);
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.repository.delete(id);
+  }
+}
